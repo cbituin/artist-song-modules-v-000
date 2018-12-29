fa6cc986fb9ed4d400ea7c0c56d6cb8f542e697b
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-12-29 19:08:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,8 +4,8 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
-  extend Memorable, Findable
-  include Paramable
+extend Memorable::ClassMethods
+include Memorable::InstanceMethods, Paramable
   
   @@artists = []
 
